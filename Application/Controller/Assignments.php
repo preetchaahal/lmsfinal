a749d5bbec088c5b47d912484f383823dc9d27f8
Harpreet Singh <singhhrpreet@outlook.com>
2016-11-21 19:50:14 +0530

    Yo Made some changes. And new Controllers. more to come

---
+++ Application/Controller/Assignments.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controller;
+
+class Assignments {
+  public function all()
+  {
+
+  }
+  public function update()
+  {
+    
+  }
+}
---
+++ Application/Controller/Students.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controller;
+
+class Students {
+  public function all()
+  {
+
+  }
+  public function update()
+  {
+    
+  }
+}
